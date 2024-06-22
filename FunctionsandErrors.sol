9f16bd661762586ee5378abedc0be9dfdaad982a
SecreShall <164751004+SecreShall@users.noreply.github.com>
2024-06-22 13:59:14 +0800

    Update FunctionsandErrors.sol

--- FunctionsandErrors.sol
+++ FunctionsandErrors.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
 contract Contract {
     uint256 public balance;
     address public owner;
